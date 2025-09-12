daa2f3bda79daa21cccca9fb952dcf8783bb3c67
Princip Gym Admin <admin@principgym.com>
2025-09-12 18:47:38 +0200

    Add SessionProvider to fix useSession hook error

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,25 +1,28 @@
 "use client"
 
 import { ThemeProvider } from "next-themes"
+import { SessionProvider } from "next-auth/react"
 import { Toaster } from "sonner"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-      <Toaster 
-        position="top-center"
-        richColors
-        closeButton
-        toastOptions={{
-          className: "font-medium",
-        }}
-      />
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+        <Toaster 
+          position="top-center"
+          richColors
+          closeButton
+          toastOptions={{
+            className: "font-medium",
+          }}
+        />
+      </ThemeProvider>
+    </SessionProvider>
   )
 }
